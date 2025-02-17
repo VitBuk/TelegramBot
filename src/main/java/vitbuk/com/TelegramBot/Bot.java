354f17814e99fa7dbd1c2585b2d057341dd3edb7
VitBuk <vitbuk@gmail.com>
2025-02-17 20:35:07 +0200

    testing interface LongPollingUpdateConsumer and overriding consume methods

--- src/main/java/vitbuk/com/TelegramBot/Bot.java
+++ src/main/java/vitbuk/com/TelegramBot/Bot.java
@@ -1,7 +1,26 @@
 package vitbuk.com.TelegramBot;
 
-public class Bot {
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+import java.util.List;
+
+public class Bot implements LongPollingSingleThreadUpdateConsumer {
 
     static final String TOKEN = Constants.BOT_TOKEN;
     static final String USERNAME = Constants.BOT_USERNAME;
+
+
+    @Override
+    public void consume(List<Update> updates) {
+        LongPollingSingleThreadUpdateConsumer.super.consume(updates);
+    }
+
+    @Override
+    public void consume(Update update) {
+        
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            System.out.println(update.getMessage().getText());
+        }
+    }
 }
