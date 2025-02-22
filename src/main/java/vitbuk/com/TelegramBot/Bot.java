ee6e99c9225c389306b2ca3d08c3983808ed4cd0
VitBuk <vitbuk@gmail.com>
2025-02-22 16:58:30 +0200

    C:/Program Files/Git/markup command which sow user keyboard of buttons added, /hide command which hide keyboard added

--- src/main/java/vitbuk/com/TelegramBot/Bot.java
+++ src/main/java/vitbuk/com/TelegramBot/Bot.java
@@ -8,6 +8,9 @@ import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.PhotoSize;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardRemove;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.generics.TelegramClient;
 
@@ -46,7 +49,58 @@ public class Bot implements LongPollingSingleThreadUpdateConsumer {
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
-            //other cases, where we dont understand user
+
+            //if user use /markup command
+        } else if (messageText.equals("/markup")) {
+            SendMessage message = SendMessage // Create a message object object
+                    .builder()
+                    .chatId(chatId)
+                    .text("Here is your keyboard")
+                    .build();
+
+            // Add the keyboard to the message
+            message.setReplyMarkup(ReplyKeyboardMarkup
+                    .builder()
+                    // Add first row of 3 buttons
+                    .keyboardRow(new KeyboardRow("Caesar", "Row 1 Button 2", "Row 1 Button 3"))
+                    // Add second row of 3 buttons
+                    .keyboardRow(new KeyboardRow("Row 2 Button 1", "Row 2 Button 2", "Row 2 Button 3"))
+                    .build());
+            try {
+                telegramClient.execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+            // when user tries something that is not a command
+        } else if (messageText.equals("Caesar")) {
+            // Send a picture to the user
+            SendPhoto msg = SendPhoto
+                    .builder()
+                    .chatId(chatId)
+                    // This time will send the picture using a URL
+                    .photo(new InputFile(Constants.CAESAR_PIC_URL))
+                    .caption("Clicked button: " + messageText)
+                    .build();
+            try {
+                telegramClient.execute(msg); // Call method to send the photo
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+
+        } else if (messageText.equals("/hide")) {
+            // Hide the keyboard
+            SendMessage message = SendMessage
+                    .builder()
+                    .chatId(chatId)
+                    .text("Keyboard hidden")
+                    .replyMarkup(new ReplyKeyboardRemove(true,false))
+                    .build();
+
+            try {
+                telegramClient.execute(message); // Call method to send the photo
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         } else {
             SendMessage message = SendMessage
                     .builder()
